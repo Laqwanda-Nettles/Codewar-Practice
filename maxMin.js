153d4852ad905e75c5e791d68a294dfff158acbb
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-06-21 21:49:18 -0500

    Create two arrow functions, one to find the minium value in array and another to find the maximum value.

---
+++ maxMin.js
@@ -0,0 +1,11 @@
+// Find Maximum and Minimun Values of a List
+
+// return min value
+const min = (list) => {
+  return Math.min.apply(null, list);
+};
+
+// return max value
+const max = (list) => {
+  return Math.max.apply(null, list);
+};
